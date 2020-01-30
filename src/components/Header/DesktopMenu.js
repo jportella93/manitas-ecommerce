749ac0e948f66bd5157569b5d7f05c01d8b39cb2
jportella93 <jportella93@gmail.com>
2020-01-30 08:24:07 +0100

    feat: replace logo

--- src/components/Header/DesktopMenu.js
+++ src/components/Header/DesktopMenu.js
@@ -21,7 +21,7 @@ const DesktopMenu = ({location: {pathname}, token, cartCount, signout}) => {
           header
         >
           <Logo />
-          Starter Store
+          1312.cat
         </Menu.Item>
         {token ? (
           <Menu.Menu position="right">
--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {Image} from 'semantic-ui-react'
+// import {Image} from 'semantic-ui-react'
 
-import moltin from '../../images/moltin-light-hex.svg.svg'
+// import moltin from '../../images/moltin-light-hex.svg.svg'
 
 const Logo = () => (
-  <Image
-    size="mini"
-    src={moltin}
-    style={{marginRight: '1.5em'}}
-    alt="I love Lamp"
-  />
+  // <Image
+  //   size="mini"
+  //   src={moltin}
+  //   style={{marginRight: '1.5em'}}
+  //   alt="I love Lamp"
+  // />
+  <span role="img" aria-label="Manita">
+    🖕
+  </span>
 )
 
 export default Logo
